556bd1ef6409f527c3f38f4629ec2755187cc3b4
Prima Translation <80651168+slavawebkiev@users.noreply.github.com>
2022-05-06 22:42:15 +0300

    Add files via upload

---
+++ cas.php
@@ -0,0 +1,63 @@
+<?php
+
+class CAS {
+	
+	private $key = '';
+
+	public function check_key(){
+		if( $this->key == '' ) return false;
+		else return true;
+	}
+
+	public function set_key( $key = '' ){
+		$this->key = $key;
+	}
+
+	public function translate( $from = '', $to = '', $text = '' ){
+		if( $this->check_key() == false ) return ['result'=>false, 'desc'=>'Not set key'];
+		if( $from == '' || $to == '' || trim( $text ) == '' ) return ['result'=>false, 'desc'=>'Incomplete query'];
+		return $this->query( 'v2', [ 'from'=>$from, 'to'=>$to, 'text'=> $text ] );
+	}
+
+	public function langs(){
+		if( $this->check_key() == false ) return ['result'=>false];
+		return $this->query( 'v2/langs' );
+	}
+
+	public function query( $path = 'v2', $data = false ) {
+		$curl = curl_init();
+		curl_setopt_array($curl, [
+			CURLOPT_URL => "https://api.cloudapi.stream/" . $path,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_MAXREDIRS => 10,
+			CURLOPT_TIMEOUT => 30,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => ( ( is_array( $data ) == true ) ? json_encode($data) : "" ),
+			CURLOPT_HTTPHEADER => [
+				"Content-Type: application/json",
+				"X-Key: " . $this->key
+			],
+		]);
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		curl_close($curl);
+		if ($err) {
+			return ['result'=>false];
+		} else {
+			return json_decode($response,true);
+		}
+	}
+  }
+
+
+?>
+
+
+
+
+
+
+
